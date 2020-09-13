0cfa1a72b756015677cfaebf251f21fe11e70f88
mac <kumaranmani92@gmail.com>
2020-09-14 03:06:25 +0530

    latest

--- src/main/java/com/product/service/EmailService.java
+++
@@ -1,5 +0,0 @@
-package com.product.service;
-
-public interface EmailService {
-	public boolean sentEmail(String host,String port,boolean auth, boolean tlsEnable, String from,String password, String to, String sub,String msg) ;
-}
